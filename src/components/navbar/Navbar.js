b9c2432a641f8d904a650e3a5dc0877f447e9e53
victory-nwawunze <victorynwawunze@gmail.com>
2023-07-11 11:43:32 +0100

    finish navbar component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,19 +1,21 @@
 import React from "react";
 import Logo from '../../images/paypal-logo-removebg-preview.png'
 import "./Navbar.css"
+import { AiOutlineDown } from 'react-icons/ai';
 function Navbar() {
   return (
     <div>
         <div className="navbar-container-style">
             <div className="navbar-flex-style">
                 <img src={Logo} className="Logo-style" alt="Logo" />
-                <ul>
+                <ul className="navbar-link">
                     <li>Personal</li>
+                    <AiOutlineDown className="Down-Icon-style" alt="Down-icon" />
                 </ul>
                 <div className="navbar-btn-container-style">
                         <div className="navbar-btn-flex-style">
-                            <button>Login</button>
-                            <button>Sign up</button>
+                            <button className="login-btn-style">Log in</button>
+                            <button className="sign-up-btn-style">Sign up</button>
                         </div>
                 </div>
             </div>
