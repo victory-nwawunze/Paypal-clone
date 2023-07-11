8d906917ed8ecaec3d345b091535cc58ad1c4c9c
victory-nwawunze <victorynwawunze@gmail.com>
2023-07-11 12:58:15 +0100

    finish navbar component to jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Logo from "./images/paypal-logo-removebg-preview.png"
 
 function App() {
   return (
     <div className="App">
- <Navbar />
+ <Navbar logo={Logo} navText="Personal"  LoginBtn="Log in" signUpBtn="Sign up" />
     </div>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,21 +1,23 @@
 import React from "react";
-import Logo from '../../images/paypal-logo-removebg-preview.png'
+
 import "./Navbar.css"
 import { AiOutlineDown } from 'react-icons/ai';
-function Navbar() {
+const Navbar = (props) => {
+    const {logo,navText,IconArrD ,LoginBtn,signUpBtn} = props
+    console.log(IconArrD)
   return (
     <div>
         <div className="navbar-container-style">
             <div className="navbar-flex-style">
-                <img src={Logo} className="Logo-style" alt="Logo" />
+                <img src={logo} className="Logo-style" alt="Logo" />
                 <ul className="navbar-link">
-                    <li>Personal</li>
+                    <li>{navText}</li>
                     <AiOutlineDown className="Down-Icon-style" alt="Down-icon" />
                 </ul>
                 <div className="navbar-btn-container-style">
                         <div className="navbar-btn-flex-style">
-                            <button className="login-btn-style">Log in</button>
-                            <button className="sign-up-btn-style">Sign up</button>
+                            <button className="login-btn-style">{LoginBtn}</button>
+                            <button className="sign-up-btn-style">{signUpBtn}</button>
                         </div>
                 </div>
             </div>
