492f0740464994048d9093c9bc4a5d2ed89f442d
victory-nwawunze <victorynwawunze@gmail.com>
2023-07-13 14:27:35 +0100

    creating signin component

--- src/components/signinForm/SigninForm.js
+++ src/components/signinForm/SigninForm.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./SigninForm.css";
 import PaypalLogo from "../../images/paypal-logo-removebg-preview.png";
+import FlagLogo from '../../images/Nigeria-Flag-icon-removebg-preview.png'
+import { AiOutlineArrowDown } from "react-icons/ai";
 const SigninForm = () => {
   return (
     <div className="signInForm-container-style">
@@ -10,11 +12,24 @@ const SigninForm = () => {
           <input placeholder="Email or mobile number" className="input-style" />
         </div>
         <div className="form-input-container">
-        <button className="forget-email-btn-style">Forget email ?</button>
+          <button className="forget-email-btn-style">Forget email ?</button>
         </div>
-          
+        <div className="form-input-container">
+          <button className="next-btn-style">Next</button>
+        </div>
+        <div className="form-input-container">
+        <button className="sign-up-btn-style">Sign Up</button>
+          </div>
+        <div className="form-input-container">
+        <div className="form-flag-container-style">
+          <div className="form-flag-flex-style">
+            <div><img src={FlagLogo} className="icon-Style" /></div>
+            <div><AiOutlineArrowDown /></div>
+          </div>
+        </div>
+          </div>
+        </div>
       </div>
-    </div>
   );
 };
 
