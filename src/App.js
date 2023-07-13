d1ee32c18caf08a40c0b2237e896b556ca530e6b
victory-nwawunze <victorynwawunze@gmail.com>
2023-07-13 11:48:30 +0100

    creating signin component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-
-import './App.css';
-import HomeLayout from './Layout/HomeLandingPageComponent/HomeLayout';
+import "./App.css";
+import HomeLayout from "./Layout/HomeLandingPageComponent/HomeLayout";
 // import HomeLayout from './components/Layout/HomeLandingPageComponent/HomeLayout';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import SigninForm from "./components/signinForm/SigninForm";
+import React from "react";
 
 function App() {
   return (
     <div>
-    <HomeLayout />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <HomeLayout />
+          </Route>
+          <Route path="/signIn">
+            <SigninForm />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
