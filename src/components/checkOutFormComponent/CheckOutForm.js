95014f8afc18b5c52b51f57b32ca123056a065ef
victory-nwawunze <victorynwawunze@gmail.com>
2023-07-15 08:34:24 +0100

    creating checkout form

--- src/components/checkOutFormComponent/CheckOutForm.js
+++ src/components/checkOutFormComponent/CheckOutForm.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./CheckOutForm.css";
 import CheckoutSpinner from "../checkoutSpinner/CheckoutSpinner";
+import MyPaypal from '../../images/myPaypal-removebg-preview.png'
 const CheckOutForm = () => {
     // Loading state
   const [isLoading, setIsLoading] = useState(true);
@@ -18,8 +19,17 @@ const CheckOutForm = () => {
       {isLoading ? (
         <CheckoutSpinner />
       ) : (
-        <div className="checkout-container-style">
-          <h1>Hello</h1>
+        <div className="checkout-bg">
+       <div className="checkout-form">
+        <img src={MyPaypal} alt="MyPaypal-Logo" className="MyPaypal-style"/>
+        <h2 className="checkout-signUp-text">Sign up for a Business <br />account</h2>
+        <p className="checkout-text-style">Enter the email address you’ll use to sign up<br /> or log in</p>
+        <input
+                  placeholder="Email Adress"
+                  className="Checkout-signUp-input-style"
+                />
+                <button className="continue-btn-style">Continue</button>
+       </div>
         </div>
       )}
     </div>
