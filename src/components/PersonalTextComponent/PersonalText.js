603c40a7943acd21b23697deb9b0fd2c1dfb57e4
victory-nwawunze <victorynwawunze@gmail.com>
2023-07-16 16:39:14 +0100

    creating Personal form

---
+++ src/components/PersonalTextComponent/PersonalText.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./PersonalText.css";
+const PersonalText = () => {
+  return (
+    <div className="paypal-text-style">
+      <h3>
+        With PayPal you can shop and send payments with
+        <br /> just one account.
+      </h3>
+      <h2>Join over 192M active account worldwide who use paypal to</h2>
+    </div>
+  );
+};
+
+export default PersonalText;
